411e5ea1107a73ab1c75733b7ce2471bb39e472a
Shanu Sharma <shanu5544@gmail.com>
2023-12-08 06:04:50 +0530

    chore(email-subscibe): fix the layout and make it again from scratch.

--- app/email-subscription/page.tsx
+++ app/email-subscription/page.tsx
@@ -4,30 +4,39 @@ import Image from "next/image";
 const EmailSubscription = () => {
   return (
     <>
-      {/* Overall Container for the page */}
-      <div className="flex justify-center items-center bg-zinc-700 h-screen">
-        {/* Container for the card */}
-        <div className="flex flex-col rounded-xl bg-zinc-800 p-2 md:flex-row space-y-4 pb-10">
-          <img
-            src="/image.jpg"
-            alt="veg-bowl"
-            className="rounded-xl h-80 md:h-60 md:rounded-l-xl md:rounded-r-none hover:scale-105 duration-200 object-fit"
-          />
-          <div className="mt-4 p-4 md:p-12 text-white text-center font-serif text-xl ">
-            Get diet and fitness tips in your inbox.
+      <div className="flex justify-center items-center h-screen bg-zinc-700">
+        <div className="p-2 bg-zinc-800 rounded-xl space-y-2">
+          {/* Container for the Image */}
+          <div>
+            <img
+              src="/image.jpg"
+              alt=""
+              className="h-80 hover:scale-105 duration-200 rounded-xl"
+            />
           </div>
-          <div className="text-white text-center text-sm max-w-sm">
-            Eat better and exercise better. Sign up for the Diet&Fitness
-            newsletter.
+          {/* Container for the content */}
+          <div className="p-8">
+            <div className="flex flex-col space-y-4">
+              <div className="font-serif text-white text-xl text-center">
+                Get diet and fitness tips in your inbox
+              </div>
+
+              <div className="font-sans text-white text-sm max-w-xs text-center">
+                Eat better and exercise better. Sign up for the Diet&Fitness
+                newsletter.
+              </div>
+
+              <input
+                type="email"
+                className="bg-zinc-800 text-sm border border-zinc-600 px-4 py-3 text-white placeholder:text-xs placeholder:text-center focus:outline-none"
+                placeholder="Enter your email address"
+              />
+
+              <div className="bg-lime-400 py-2 px-4 rounded-lg text-zinc-700 text-center hover:bg-lime-700 hover:text-white">
+                Subscribe
+              </div>
+            </div>
           </div>
-          <input
-            type="email"
-            placeholder="Enter your email address"
-            className="bg-zinc-800 border border-zinc-500 p-2 m-4 font-sans placeholder:text-center placeholder:text-sm focus:outline-none"
-          />
-          <button className="bg-lime-500 rounded-xl p-2 m-4 text-zinc-800 text-sm">
-            Subscribe
-          </button>
         </div>
       </div>
     </>
